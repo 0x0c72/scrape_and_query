bc7c127882b2028162e48bd000b6739ceee9a35f
Chris Shenkan <cshenkan@shenkan-associates.com>
2014-05-09 18:56:55 -0400

    Remove some files

--- hitbot2.py
+++ hitbot2.py
@@ -11,7 +11,38 @@ import datetime
 from bs4 import BeautifulSoup
 import time
 import sys
-import yes_no_query
+
+def query_yes_no(question, default="yes"):
+	"""Ask a yes/no question via raw_input() and return their answer.
+
+	"question" is a string that is presented to the user.
+	"default" is the presumed answer if the user just hits <Enter>.
+		It must be "yes" (the default), "no" or None (meaning
+		an answer is required of the user).
+
+	The "answer" return value is one of "yes" or "no".
+	"""
+	valid = {"yes":True,   "y":True,  "ye":True,
+			 "no":False,     "n":False}
+	if default == None:
+		prompt = " [y/n] "
+	elif default == "yes":
+		prompt = " [Y/n] "
+	elif default == "no":
+		prompt = " [y/N] "
+	else:
+		raise ValueError("invalid default answer: '%s'" % default)
+
+	while True:
+		sys.stdout.write(question + prompt)
+		choice = raw_input().lower()
+		if default is not None and choice == '':
+			return valid[default]
+		elif choice in valid:
+			return valid[choice]
+		else:
+			sys.stdout.write("Please respond with 'yes' or 'no' "\
+							 "(or 'y' or 'n').\n")
 
 now = datetime.datetime.now()
 currentDate = [str(now.month), str(now.day), str(now.year)]
