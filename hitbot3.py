f2adc26c41bd9474312a18081dfdc8c07d624809
Chris Shenkan <cshenkan@shenkan-associates.com>
2014-05-09 00:07:30 -0400

    some refactoring

--- hitbot3.py
+++ hitbot3.py
@@ -22,9 +22,10 @@ def make_request(url):
 def get_test_url():
 	try:
 		print "Enter a url to be scraped..."
-		print 'Usage  - "http://phocks.org/stumble/creepy/" <-- With the double quotes'
+		print 'Usage  - "http://google.com/" <-- With the double quotes'
 		print "'Q' or 'q' to quit"
 		url = raw_input("@> ")
+
 		if url.lower() == "q":
 			sys.exit(1)
 	except NameError, n1:
@@ -33,9 +34,10 @@ def get_test_url():
 	except SyntaxError, s1:
 		print "Error\nDescription %s" % s1
 		get_test_url()
-	url = eval(url)
+
+	url = eval(url) 
 	res = requests.get(url)
-	print res.status_code
+
 	if 300 <= int(res.status_code) > 400:
 		print "Request failed: Response code: %d" % int(res.status_code)
 		get_test_url()
@@ -43,7 +45,7 @@ def get_test_url():
 		print "Request failed: Response code: %d" % int(res.status_code)
 		get_test_url()
 	elif 200 <= int(res.status_code) < 300:
-		print "Success!\nStatus Code: %d" % int(res.status_code)
+		print "Success!\nURL %s, returned Status Code: %d" % (str(url), int(res.status_code))
 	else:
 		print "Request failed: Reason Unknown"
 		get_test_url()
